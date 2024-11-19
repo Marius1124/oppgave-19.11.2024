ab3e84f74a58c261712ab437f2a346dff72d6cae
Marius <blokma1124@gmail.com>
2024-11-19 15:09:18 +0100

    Struktur forandring i oppgave 1

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ let userName = "John";
 let userAge = 20;
 let userCity = "Bergen";
 
-if (userAge > 18) {
-  console.log("Du er over 18, velkommen inn");
+if (userName === "John" && userCity === "Bergen") {
+  console.log("Velkommen inn, John fra Bergen!");
 } else if (userAge <= 18 || userCity === "Oslo") {
   console.log("Du er ikke gammel nok, og du er fra feil by");
-} else userName === "John" && userCity === "Bergen";
-{
-  console.log("Velkommen inn, John fra Bergen!");
+} else if (userAge > 18) {
+  console.log("Du er over 18, velkommen inn!");
+} else {
+  console.log("Default");
 }
 
 // Oppgave 2
